175b2dd0630d7e91343201ddbc56970546f2a9d3
armin <genius2001armin@gmail.com>
2023-03-10 13:51:59 +0330

    Add Seeder for User and Product

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        // \App\Models\UserSeeder::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
+        // \App\Models\UserSeeder::factory()->create([
+        //     'name' => 'Test UserSeeder',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call([
+            ProductSeeder::class,
+            UserSeeder::class,
+        ]);
     }
 }
